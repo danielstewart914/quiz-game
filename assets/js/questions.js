4faef1385d9a2fd6ae8a69e34e6f10e6f4e50243
danielstewart914 <103592919+danielstewart914@users.noreply.github.com>
2022-06-16 15:11:04 -0700

    Updated questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,20 +1,69 @@
 var questions = [
 {
-    question: 'Test Question',
-    answers: [ '1 Test', '2 Test', '3 Test', '4 Test' ],
-    correctAnswer: '1 Test'
+    question: 'JavaScript is a _____-side programming language.',
+    answers: [ 'Client', 'Server', 'Both', 'None' ],
+    correctAnswer: 'Both'
 },
 
 {
-    question: 'Test Question 2',
-    answers: [ 'Bob', 'Larry', 'Patrick', 'Orange' ],
-    correctAnswer: 'Orange'
+    question: 'Which of the following will write "Hello World" in an alert box?',
+    answers: [ 'alertBox(“Hello World!”);', 'alert(“Hello World!”);', 'msgAlert(“Hello World!”);', 'alert(Hello World!);' ],
+    correctAnswer: 'alert(“Hello World!”);'
 },
 
 {
-    question: 'Test Question 3',
+    question: 'Which method would you use to find the minimum of x and y using JavaScript?',
+    answers: [ 'Math.min(x,y)', 'min(x,y)', 'min(xy)', 'Math.min(xy)' ],
+    correctAnswer: 'Math.min(x,y)'
+},
+
+{
+    question: 'Which "if" statement is correct to execute if "x" is equal to 2?',
+    answers: [ 'if(x 2)', ' if(x = 2)', 'if(x != 2 )', 'if(x === 2)' ],
+    correctAnswer: 'if(x === 2)'
+},
+
+{
+    question: 'JavaScript has a file extension of:',
+    answers: [ '.java', '.js', '.javascript', '.xml' ],
+    correctAnswer: '.js'
+},
+
+{
+    question: 'Which of the following is true about variable naming conventions in JavaScript?',
+    answers: [ 'JavaScript variable names must begin with a letter or underscore', 'JavaScript variable names are case sensitive', 'Both of the above', ' None of the above' ],
+    correctAnswer: 'Both of the above'
+},
+
+{
+    question: 'Which symbol is used separate JavaScript statements?',
+    answers: [ 'Comma', 'Colon', 'Hyphen', 'Semicolon' ],
+    correctAnswer: 'Semicolon'
+},
+
+{
+    question: 'Which JavaScript keyword is used to declare a variable?',
+    answers: [ 'Var', 'var', 'Let', 'All of the above' ],
+    correctAnswer: 'var'
+},
+
+{
+    question: 'How many keywords are there in JavaScript to declare variables or constants?',
     answers: [ '1', '2', '3', '4' ],
-    correctAnswer: '2'
+    correctAnswer: '3'
+},
+
+{
+    question: 'What is the default value of an uninitialized variable?',
+    answers: [ '0', 'undefined', 'null', 'NaN' ],
+    correctAnswer: 'undefined'
 }
 
 ];
+
+var objectTemplate = 
+{
+    question: '',
+    answers: [ '', '', '', '' ],
+    correctAnswer: ''
+};
