c7ac8a6de136935e467410cfa350b1ede4cd8b98
danielstewart914 <103592919+danielstewart914@users.noreply.github.com>
2022-06-16 11:57:24 -0700

    Added event listener for starting game

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,21 +4,38 @@ var welcomeScreenEl = document.querySelector( '#welcome-screen' );
 var questionBoxEl = document.querySelector( '#question-box' );
 var endScreenEl = document.querySelector( '#end-screen' );
 var responseBoxEl = document.querySelector( '#response-box' );
+var timerEl = document.querySelector( '#timer' );
 
 // other global variables
 var questionPointer = 0;
 var currentQuestion = questions[questionPointer];
 var timerInterval;
-var countdown = 75;
+var countdown = 10;
 var score = 0;
 
 const oneSecond = 1000;
 
-function startTimer() {
+function startTimer () {
+    
+    timerInterval = setInterval( function () {
 
+        countdown--;
+
+        timerEl.textContent = countdown;
+
+        if ( !countdown ) {
+            clearInterval( timerInterval );
+
+            questionBoxEl.innerHTML = '';
+
+            printEndScreen();
+        }
+
+    }, oneSecond );
 }
 
 function printQuestion () {
+    // print question in h2 header and create 4 buttons from the question object
     questionBoxEl.innerHTML =  `
         <h2>${currentQuestion.question}</h2>
         <button data-answer="${currentQuestion.answers[0]}" class="answer-button button">${currentQuestion.answers[0]}</button>
@@ -29,12 +46,19 @@ function printQuestion () {
 }
 
 function printEndScreen () {
-
+    
 }
 
-printQuestion();
-
-
 gameBoxEl.addEventListener( 'click', function ( event ) {
+
+    if ( event.target.id === 'start-button' ) {
+
+        // clear welcome screen
+        welcomeScreenEl.innerHTML = '';
+
+        printQuestion();
+        startTimer();
+
+    }
 
 } );
