3951013de421d2540f65777ba069856d2241f6da
danielstewart914 <103592919+danielstewart914@users.noreply.github.com>
2022-06-19 08:39:09 -0700

    reconfigured game start and added initial code for high score submission

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,14 +4,17 @@ var welcomeScreenEl = document.querySelector( '#welcome-screen' );
 var questionBoxEl = document.querySelector( '#question-box' );
 var endScreenEl = document.querySelector( '#end-screen' );
 var responseBoxEl = document.querySelector( '#response-box' );
+var submitButtonEl = document.querySelector( '#submit-high-score' );
+var highScoreResponseEl = document.querySelector( '#high-score-display' );
 var scoreEl = document.querySelector( '#score-box' );
 var timerEl = document.querySelector( '#timer' );
 
 // other global variables
-var questionPointer = 0;
-var currentQuestion = questions[questionPointer];
+var highsScores = JSON.parse( localStorage.getItem( 'highScores' ) );
+var questionPointer;
+var currentQuestion;
 var timerInterval;
-var countdown = 30;
+var countdown;
 var score = 0;
 
 const oneSecond = 1000;
@@ -33,7 +36,6 @@ function startTimer () {
 
         }
 
-
         // if timer runs out clear question box amd print end screen
         if ( countdown <= 0 ) {
             clearInterval( timerInterval );
@@ -42,7 +44,7 @@ function startTimer () {
 
             questionBoxEl.classList.add( 'hide' );
 
-            printEndScreen();
+            displayEndScreen();
         }
 
     }, oneSecond );
@@ -50,33 +52,33 @@ function startTimer () {
 
 function printQuestion () {
 
-    // print question in h2 header and create 4 buttons from the question object
+    // display question in h2 header and create 4 buttons from the current question object
     questionBoxEl.innerHTML =  `
         <h2>${currentQuestion.question}</h2>
-        <button data-answer="${currentQuestion.answers[0]}" class="answer-button button">${currentQuestion.answers[0]}</button>
-        <button data-answer="${currentQuestion.answers[1]}" class="answer-button button">${currentQuestion.answers[1]}</button>
-        <button data-answer="${currentQuestion.answers[2]}" class="answer-button button">${currentQuestion.answers[2]}</button>
-        <button data-answer="${currentQuestion.answers[3]}" class="answer-button button">${currentQuestion.answers[3]}</button>
+        <button data-answer="${currentQuestion.answers[0]}" class="answer button">${currentQuestion.answers[0]}</button>
+        <button data-answer="${currentQuestion.answers[1]}" class="answer button">${currentQuestion.answers[1]}</button>
+        <button data-answer="${currentQuestion.answers[2]}" class="answer button">${currentQuestion.answers[2]}</button>
+        <button data-answer="${currentQuestion.answers[3]}" class="answer button">${currentQuestion.answers[3]}</button>
     `;
 
+    // display updated score
     scoreEl.innerHTML = 'Score: ' + score;
 }
 
-function printEndScreen () {
+function displayEndScreen () {
 
     // pulse timer and score
     timerEl.innerHTML = `<span class="pulse">${countdown}</span>`;
-    scoreEl.innerHTML = `Score: <span class="pulse">${score}</span>`;
+    scoreEl.innerHTML = `<span class="pulse">Score: ${score}</span>`;
 
-
-    var addScoreTimer = setInterval( function () {
+    // transfer remaining time to score
+    var addTimerToScore = setInterval( function () {
 
         if ( countdown <= 0 ) {
         
-            clearInterval( addScoreTimer );
+            clearInterval( addTimerToScore );
 
-            // hide question box
-            questionBoxEl.classList.add( 'hide' );
+            // reveal end screen
             endScreenEl.classList.remove( 'hide' );
 
         } else {
@@ -85,7 +87,7 @@ function printEndScreen () {
             score++;
 
             timerEl.children[0].textContent = countdown;
-            scoreEl.children[0].textContent = score;
+            scoreEl.children[0].textContent = 'Score: ' + score;
 
         }
 
@@ -97,10 +99,23 @@ function printEndScreen () {
 gameBoxEl.addEventListener( 'click', function ( event ) {
 
     // if the start button is clicked start game
-    if ( event.target.id === 'start-button' ) {
+    if (  event.target.classList.contains( 'start' ) ) {
 
-        // clear welcome screen
+        // clear welcome screen and display questions
         welcomeScreenEl.classList.add( 'hide' );
+        endScreenEl.classList.add( 'hide' );
+        questionBoxEl.classList.remove( 'hide' );
+
+
+        // pointer to first question
+        questionPointer = 0;
+
+        // set initial value of question
+        currentQuestion = questions[questionPointer];
+
+        countdown = 30;
+
+        score = 0;
 
         // print first question
         printQuestion();
@@ -109,9 +124,7 @@ gameBoxEl.addEventListener( 'click', function ( event ) {
     }
 
     // if you an answer button is clicked check against the correct answer
-    if (  event.target.classList.contains( 'answer-button' ) ) {
-
-        console.log( 'answer button clicked' );
+    if (  event.target.classList.contains( 'answer' ) ) {
 
         var clickedAnswer = event.target.dataset.answer;
 
@@ -137,21 +150,60 @@ gameBoxEl.addEventListener( 'click', function ( event ) {
 
         questionPointer++;
 
-        // if we reach the end of the question array end game else continue
+        // if we have reached the end of the question array end game otherwise continue
         if ( questionPointer === questions.length ) {
 
             // stop timer
             clearInterval( timerInterval );
+            
+            // hide question box
+            questionBoxEl.classList.add( 'hide' );
 
-            // print the end game screen
-            printEndScreen();
+            // display the end game screen
+            displayEndScreen();
 
         } else {
-            // print next question to the screen
+
+            // get next question and print it to the screen
             currentQuestion = questions[questionPointer];
             printQuestion();
 
         }
     }
+
+} );
+
+// listen for high score
+submitButtonEl.addEventListener( 'click', function (event) {
+
+    event.preventDefault();
+    event.stopPropagation();
+
+    //get initials from form
+    var enteredInitials = document.querySelector( '#initials' ).value;
+
+    // if blank make initials 'MVP'
+    if ( !enteredInitials ) {
+
+        enteredInitials = 'MVP';
+    }
+
+    // if no highscore are entered
+    if ( !highsScores ) {
+        
+        console.log( 'yes' );
+    
+    }
+
+    var highScore = {
+        initials: enteredInitials.toUpperCase(),
+        score: score
+    };
+
+    // display high thank you and high score submission
+    highScoreResponseEl.innerHTML = `
+        <h3>${ enteredInitials } - ${ score }</h3>
+        Thank you. Your high score has been saved.
+    `;
 
 } );
